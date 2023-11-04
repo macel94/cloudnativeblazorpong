5ec8860f689abfbede5ed102b040d155fa764e91
francesco.belacca <francesco.belacca@agic.cloud>
2023-11-04 16:55:36 +0100

    started transporting the PongComponent

---
+++ src/BlazorPong.Web/Shared/ClientType.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPong.Web.Shared;
+
+public enum ClientType
+{
+    Spectator = 0,
+    Player1 = 1,
+    Player2 = 2
+}
---
+++ src/BlazorPong.Web/Shared/GameObject.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorPong.Web.Shared;
+
+public record GameObject(double Left, double Top, string Id, string LastUpdatedBy, int Width, int Height, long? LastUpdateTicks = 0)
+{
+    public bool Draggable { get; set; }
+    public long LastSentUpdate = 0;
+    public bool NeedsToSendUpdate => LastUpdateTicks > LastSentUpdate;
+    public string LeftPx => $"{(int)Left}px";
+
+    public string TopPx => $"{(int)Top}px";
+}
