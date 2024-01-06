7a935b89dd0ce797c0325da576a9228f9b413dc1
francesco.belacca <francesco.belacca@agic.cloud>
2024-01-06 18:26:05 +0100

    fixing #4 and #5

--- src/BlazorPong.Web/Client/Program.cs
+++ src/BlazorPong.Web/Client/Program.cs
@@ -1,13 +1,16 @@
-﻿using BlazorPong.Web.Client;
-using BlazorPong.Web.Shared.Clock;
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using BlazorPong.Web.Client;
+using BlazorPong.Web.Shared.Clock;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<ISystemClock, SystemClock>();
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
