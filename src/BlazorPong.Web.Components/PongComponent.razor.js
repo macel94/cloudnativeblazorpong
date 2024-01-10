a88dfa4eb6ec6699d79a3c7e1845c3ddbc055499
francesco.belacca <francesco.belacca@agic.cloud>
2024-01-10 14:52:59 +0100

    now using Ilogger<component>

--- src/BlazorPong.Web.Components/PongComponent.razor.js
+++ src/BlazorPong.Web.Components/PongComponent.razor.js
@@ -8,10 +8,6 @@ export function unsetOnbeforeunload(instance) {
     window.onbeforeunload = null;
 };
 
-export function log(message) {
-    console.debug(message);
-};
-
 export function getContainerHeight() {
     var element = document.getElementById('gamearea');
     if (element) {
