5784127ca5348513ad042204a41fb70fff2ab8f5
francesco.belacca <francesco.belacca@agic.cloud>
2024-01-12 12:05:07 +0100

    deleted unused files, commented out second instance

--- src/Blazorpong.Database.AzureSql/Scripts/deploy-dacpac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Wait for the SQL Server to come up
-#sleep 15s
-
-# Deploy DACPAC to the database
-#/opt/sqlpackage/sqlpackage /a:Publish /tsn:azuresql /tdn:pongfreesqlserver /tu:sa /tp:$SA_PASSWORD /sf:/v0.dacpac
