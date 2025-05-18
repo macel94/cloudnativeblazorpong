5a22d2073f59b59e6abb55c464bb4d473e660647
Francesco Belacca <francesco.belacca@hotmail.it>
2025-05-18 10:22:11 +0200

    changes for playwright and timeout

--- src/tests-e2e/tests/room.spec.ts
+++ src/tests-e2e/tests/room.spec.ts
@@ -1,23 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-test('navigate to room and generate new room', async ({ page }) => {
+test('navigate to room, generate new room, try to play with 2 players', async ({ page }) => {
   await page.goto('http://localhost:6350/room');
 
   // Click the "generate new room" button.
@@ -28,14 +11,23 @@ test('navigate to room and generate new room', async ({ page }) => {
 
   // Take a screenshot.
   await page.screenshot({ path: 'screenshot1.png' });
-
+  
   // Copy the current URL.
   const currentUrl = page.url();
 
   // Open a new browser tab and go to the copied URL.
   const newPage = await page.context().newPage();
   await newPage.goto(currentUrl);
-
+  
   // Take another screenshot of the second browser session.
   await newPage.screenshot({ path: 'screenshot2.png' });
+
+  await page.getByRole('button', { name: 'Play' }).click();
+  await newPage.getByRole('button', { name: 'Play' }).click();
+  
+  // Take another screenshot of the second browser session.
+  await page.screenshot({ path: 'screenshot1_2.png' });
+
+  // Take another screenshot of the second browser session.
+  await newPage.screenshot({ path: 'screenshot2_2.png' });
 });
